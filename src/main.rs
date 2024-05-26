a53a93c5a3a90e5a148d8b4f2ee1496533be853e
zhouzhangqi <1978892854@qq.com>
2024-05-26 16:36:30 +0800

    feature: testcode for anyhow/thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, mem::size_of};
+
+use anyhow::Context;
+use ecosystem::MyError;
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("size of anyhow::Error is {}", size_of::<anyhow::Error>());
+    println!("size of std::io::Error is {}", size_of::<std::io::Error>());
+    println!(
+        "size of std::num::ParseIntError is {}",
+        size_of::<std::num::ParseIntError>()
+    );
+    println!(
+        "size of serde_json::Error is {}",
+        size_of::<serde_json::Error>()
+    );
+    println!("size of string is {}", size_of::<String>());
+    println!("size of MyError is {}", size_of::<MyError>());
+
+    let filename = "Cargo.toml";
+    let _fd =
+        fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
+
+    fail_with_error()?;
+
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
